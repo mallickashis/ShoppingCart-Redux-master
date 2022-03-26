c24d7fd0762f6a92e5fc9cfae5961218a5282bbc
ashismallick <43181302+mallickashis@users.noreply.github.com>
2022-03-26 11:50:38 +0530

    first commit

---
+++ src/redux/Shopping/shopping-reducer.js
@@ -0,0 +1,81 @@
+import * as actionTypes from "./shopping-types";
+
+const INITIAL_STATE = {
+  products: [
+    {
+      id: 1,
+      title: "Cube",
+      description:
+        "It helps to sharp your brain , Buy to play it",
+      price: 15.0,
+      image:
+        "https://images.unsplash.com/photo-1591991731833-b4807cf7ef94?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    },
+    {
+      id: 2,
+      title: "Coffee Cup",
+      description:
+        "Get a cup of coffee every morning before the day starts",
+      price: 20.0,
+      image:
+        "https://images.unsplash.com/photo-1572119865084-43c285814d63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    },
+    {
+      id: 3,
+      title: "Books",
+      description:
+        "Best thing to keep yourself busy and growth your knowledge",
+      price: 150.0,
+      image:
+        "https://images.unsplash.com/photo-1512820790803-83ca734da794?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1374&q=80",
+    },
+  ],
+  cart: [],
+  currentItem: null,
+};
+
+const shopReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_TO_CART:
+      const item = state.products.find(
+        (product) => product.id === action.payload.id
+      );
+      const inCart = state.cart.find((item) =>
+        item.id === action.payload.id ? true : false
+      );
+
+      return {
+        ...state,
+        cart: inCart
+          ? state.cart.map((item) =>
+            item.id === action.payload.id
+              ? { ...item, qty: item.qty + 1 }
+              : item
+          )
+          : [...state.cart, { ...item, qty: 1 }],
+      };
+    case actionTypes.REMOVE_FROM_CART:
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload.id),
+      };
+    case actionTypes.ADJUST_ITEM_QTY:
+      return {
+        ...state,
+        cart: state.cart.map((item) =>
+          item.id === action.payload.id
+            ? { ...item, qty: +action.payload.qty }
+            : item
+        ),
+      };
+    case actionTypes.LOAD_CURRENT_ITEM:
+      return {
+        ...state,
+        currentItem: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default shopReducer;
